b5e3706ffbbb9c0c95bee1c93161565b324331d3
corderdevilio <coderdevil.io@gmail.com>
2025-09-08 23:33:08 +0530

    Raft Implementation with cmd, internal, benchmarks

---
+++ DS-Projects/Raft/cmd/raft-cli/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Raft CLI client")
+	// TODO: implement client commands
+}
---
+++ DS-Projects/Raft/cmd/raftd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Raft server starting...")
+	// TODO: initialize Raft node
+}
