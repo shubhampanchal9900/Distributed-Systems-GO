05b0573d004937a968be5cc0255af20739bd8952
corderdevilio <coderdevil.io@gmail.com>
2025-09-19 00:00:37 +0530

    removed unwanted code

--- Raft/cmd/raft-cli/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Raft CLI client")
-	// TODO: implement client commands
-}
--- Raft/cmd/raftd/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Raft server starting...")
-	// TODO: initialize Raft node
-}
