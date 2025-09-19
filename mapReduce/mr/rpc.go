b206d8ace76dca875c1097e2e3a5bccd67f909bf
corderdevilio <coderdevil.io@gmail.com>
2025-09-19 23:39:02 +0530

    minor changes

--- mapReduce/mr/rpc.go
+++ mapReduce/mr/rpc.go
@@ -1,5 +1,10 @@
 package mr
 
+import (
+	"os"
+	"strconv"
+)
+
 type TaskType int
 
 const (
@@ -8,11 +13,44 @@ const (
 
 	//TaskMap tells the worker to perform a map task over a single input file
 	TaskMap
-	//TaskWait tells the worker to wait for the task to be assigned
-	TaskWait
+	// //TaskWait tells the worker to wait for the task to be assigned
+	// TaskWait
 	//TaskReduce tells the worker to perform a reduce task over the intermediate files
 	TaskReduce
 
-	//TasjkExit tells the worker to exit
+	//TaskExit tells the worker to exit
 	TaskExit
 )
+
+// keyvalue is public big K ...
+// Standard pair produced by map and consumed by reduce
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+// request args i.e. the rpc requests when worker asks for a work
+type RequestArgs struct{}
+
+type RequestTaskReply struct {
+	Type    TaskType // what kind of task is this  (Map/Reduce/EXIT/nONE)
+	TaskId  int      // the unique id of the task
+	Files   []string // input file to map
+	NReduce int      // total number of reduce tasks in the job
+	NMap    int      // total number of map tasks in the job
+
+}
+
+// reportTaskArgs is sent by a worker to coordinator when it finishes a task
+type ReportArgs struct {
+	Type   TaskType // task can be map or reduce so
+	TaskId int      // id
+	Ok     bool     // finished or not finished
+}
+
+// for future
+type ReportTaskReply struct{}
+
+func coordinatorSock() string {
+	return "var/temp/ds" + strconv.Itoa(os.Getuid())
+}
--- mapReduce/mr/worker.go
+++ mapReduce/mr/worker.go
@@ -51,6 +51,104 @@ func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string)
 			//no work now :: avoid busy spinnning and re-ask later
 			time.sleep(200 * time.Millisecond)
 
+		case TaskExit:
+			// coordinator declared job finished : terminate worker loop
+              return
 		}
 	}
+}
+
+// report sends a completion or failure notification to the coordinatr
+// we ignore the RPC success here iff coordinator is gone, jobs over anyway
+
+func report(tt TaskType, id int , ok bool){
+	args := ReportTaskArgs(Type: tt, TaskID: id, OK:ok) //prepare payload
+	rep :=ReportTaskReply{}                             // empty reply struct
+	_ = call("Coordinator.RepotTask", &args,&rep)        //  notification
+}
+
+//doMap runs a single map task
+// it reads the input fully
+// call mapf to produce []KeyValue
+// partitions by bucket = ihash(key) % nReduce.
+// we write each bucket to its own json file mr-<mapID>-<reduceID> using temp+rename.
+
+func doMap(mapID int ,filename string, nReduce int, mapf func(string,string) []KeyValue) error{
+
+	//Read the entire input file into memory
+	data, err :=os.ReadFile(filename)
+	if err !=nil{
+		return err // Coordinator will reschedule on timeout or failure .we return error
+	}
+
+	// we have to run user supplied map function from plugin((((oh user gave me logic))))
+	kva := mapf(filename, string(data))
+
+	// prepare nReduce buckets: buckets[r] collects all kV whose hash maps to r.
+	buckets := make([][]KeyValue,nReduce)
+
+	for _, kv := range kva{
+		r:= ihash(kv.key) % nReduce // reduce partition for this key
+		buckets[r] =append(buckets[r],kv)
+	}
+
+	//for each reduce bucket r , write a jsob stream of keyvalue obj
+	// we will use temp file to avoid partial completion
+	for r :=0; r< nReduce; r++{
+		//create a temp file in default temp dir
+		tmp, err := os.CreateTemp("", fmt.Sprintf("mr-%d-%d-*", mapID, r))
+		if err != nil {
+			return err
+		}
+
+		//Stream-encode each KV in this bucket to JSON FILES
+		enc := json.NewEncoder(tmp)
+		for _, kv := range buckets[r]{
+	// On any encode error, close and propagate error so coordinator can reschedule.
+			if err := enc.Encode(&kv) ; err !=nil{
+				tmp.Close()
+				return err
+			}
+		}
+
+		//ensure flush to disk and close file handle 
+		if err := tmp.Close(); err != nil{
+			return err
+		}
+
+		//Compute final filename mr-<mapID>-<reduceID>
+		final := fmt.Sprintf("mr-%d-%d",mapID,r)
+
+		//Atomically we have to rename temp file to final file 
+		//This avoids other processes reading partially written output
+		if err := os.rename(tmp.name(),final); err !=nil{
+			return err
+		}
+
+	}
+	return nil
+
+
+}
+
+//doReduce runs a single Task:
+// collects all intermediate files matching mr-*-%d for this reduceID
+// Decides JSON strams into map[key][] values
+// Sorts keys deterministically
+// ans then calls reducef for each key and writes mr-out-<reduceID> 
+func doReduce(reduceID int,reducef func(string,[]string) string) error{
+	//first discover all partitions for this reduce bucket produced by all maps
+	// local shared filesystem
+	files, _ := filepath.Glob(fmt.Sprintf("mr-*-%d",reduceID))
+
+	//kmap collects all values grouped by key across al contributing map tasks
+	kmap := make(map[string][]string)
+	for _, f: range files{
+		fd, err := os.Open(f)
+		if err !=nil{
+			return err
+		}
+		
+	}
+
 }
